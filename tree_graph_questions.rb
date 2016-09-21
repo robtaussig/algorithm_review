2b572d15e6e3a0c0f677f36e808e3ed63f4157a6
robtaussig <robert.taussig@gmail.com>
2016-09-21 16:10:33 -0400

    Finish BST and BST to linkedList

--- tree_graph_questions.rb
+++ tree_graph_questions.rb
@@ -1,6 +1,7 @@
 require_relative 'graph'
 require 'byebug'
 require_relative 'queue'
+require_relative 'linked_list'
 
 vertices = {
   a: [:b,:c,:d],
@@ -39,4 +40,61 @@ def any_route?(graph, start_node, end_node)
   false
 end
 
-# 4.2 
+# 4.2 given a sorted (increasing order) array with unique integer elements,
+# write an algorithm to create a binary search tree with minimal height
+
+=begin
+  e.g. [1,2,3,4,5,6,7,8] ==>
+
+        4
+      /   \
+     2     7
+    / \   /  \
+   1   3 5    8
+          \
+           6
+=end
+
+def build_BST(array, left=0, right=array.length-1)
+  return if left > right
+  index_mid = left + (right-left) / 2
+  node = Vertex.new(array[index_mid])
+  node.neighbors[0] = build_BST(array,left, index_mid - 1)
+  node.neighbors[1] = build_BST(array,index_mid + 1, right)
+  node
+end
+
+# p build_BST([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+
+def BST_depth_to_linked_list(array)
+  bst = build_BST(array)
+  visited = Hash.new(false)
+  values = []
+  queue = Queue.new
+  queue.enqueue(bst)
+  values << bst.value
+  until queue.is_empty?
+    node = queue.dequeue
+    unless visited[node.value]
+      node.neighbors.each do |neighbor|
+        if neighbor
+          values << neighbor.value
+          queue.enqueue(neighbor)
+        end
+      end
+    end
+    visited[node.value] = true
+  end
+  linked_lists = [LinkedList.new]
+  depth = 1
+  values.each_with_index do |val, idx|
+    if idx == (2 ** depth) - 1
+      linked_lists << LinkedList.new
+      depth += 1
+    end
+    linked_lists.last.append(val)
+  end
+  linked_lists
+end
+
+p BST_depth_to_linked_list([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
